f4bf1b860693e75ea6de174b4171d6e64f7ff700
Ganesh Jamkar <ganesh.b.jamkar@gmail.com>
2024-01-23 14:42:42 +0530

    basics of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Chai aur | React Learning Ganesh Jamkar</h1>; */}
+      <Chai />
+    </>
+  );
+}
+
+export default App;
