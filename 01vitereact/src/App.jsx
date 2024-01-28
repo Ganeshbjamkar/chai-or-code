67508877f52602e14457800020af77f847e6ff28
Ganeshbjamkar <ganesh.b.jamkar@gmail.com>
2024-01-28 19:01:40 +0530

    Counter Project using React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,14 @@
 import Chai from "./chai";
 
 function App() {
-  return <Chai />;
+  const username = "G@nesh";
+
+  return (
+    <>
+      <Chai />
+      <h1>Chai aur react {username}</h1>
+    </>
+  );
 }
 
 export default App;
